c94170f6b7c6fe84d82dd689290e043ff278c552
Punit Dekate <punitdekate.1999@gmail.com>
2024-08-05 17:11:27 +0530

    Navbar

---
+++ photo_folio/src/App.js
@@ -0,0 +1,13 @@
+import "./app.css";
+import Navbar from "./components/navbar/Navbar";
+function App() {
+  return (
+    <>
+      <div className="app-container">
+        <Navbar />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ photo_folio/src/components/navbar/Navbar.js
@@ -0,0 +1,22 @@
+import styles from "./Navbar.module.css";
+
+export default function Navbar() {
+  return (
+    <div className={styles.nav}>
+      <div className={styles.logo}>
+        <div className={styles.logo_img}>
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRnRAO1pVp0wJHG2Sv6snO35g2hH2jLIIgfmw&s"
+            alt=""
+          />
+        </div>
+        <div className={styles.logo_text}>
+          <p>PhotoFolio</p>
+        </div>
+      </div>
+      <div>
+        <ul className={styles.nav_links}></ul>
+      </div>
+    </div>
+  );
+}
